50f675591211bc111ec97795d99d47bfb0b14ff8
KarinaPavliuk <kpavliuk8@gmail.com>
2023-11-09 22:02:21 +0200

    Update PublicRoute.jsx

--- src/guards/PublicRoute/PublicRoute.jsx
+++ src/guards/PublicRoute/PublicRoute.jsx
@@ -5,7 +5,6 @@ import { Navigate, useLocation } from 'react-router-dom';
 const PublicRoute = ({ children }) => {
   const isAuth = useSelector(authSelector);
   const location = useLocation();
-  console.log('location :>> ', location);
 
   return !isAuth ? children : <Navigate to={location.state ?? '/contacts'} />;
 };
