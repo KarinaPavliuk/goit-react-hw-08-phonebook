8a3a9d3de3ea8569c37a572c5e5ed67afcbb3b16
KarinaPavliuk <kpavliuk8@gmail.com>
2023-11-09 20:01:23 +0000

    Deploying to gh-pages from @ KarinaPavliuk/goit-react-hw-08-phonebook@f89bd97a2bb60ab038bc77b1508731d7b48d036a 🚀

---
+++ static/js/237.00ee0215.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[237],{9237:function(e,i,l){l.r(i),l.d(i,{default:function(){return c}});var r=l(1087),n="PublicPage_list__ed3EY",s="PublicPage_item__TZHP4",t=l(3329),c=function(){return(0,t.jsxs)("ul",{className:n,children:[(0,t.jsx)("li",{className:s,children:(0,t.jsx)(r.rU,{to:"/register",children:"Sign up"})}),(0,t.jsx)("li",{className:s,children:(0,t.jsx)(r.rU,{to:"/login",children:"Sign in"})})]})}}}]);
+//# sourceMappingURL=237.00ee0215.chunk.js.map
